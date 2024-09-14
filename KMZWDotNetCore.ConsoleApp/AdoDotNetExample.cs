a8dbbfe44e479802172e73a1724fedfbbb3bc5cf
KaungMyatZawWin <kaungmyatzawwin.dev@gmail.com>
2024-09-14 23:30:51 +0630

    feat: read method

---
+++ KMZWDotNetCore.ConsoleApp/AdoDotNetExample.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMZWDotNetCore.ConsoleApp
+{
+    public class AdoDotNetExample
+    {
+        private readonly SqlConnectionStringBuilder _sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "DotNetTrainingBatch5",
+            UserID = "sa",
+            Password = "sasa@123"
+        };
+
+        public void Read()
+        {
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+            Console.WriteLine("Connection is open.");
+
+            string qeuryString = "select * from Tbl_Blog";
+            SqlCommand cmd = new SqlCommand(qeuryString, connection);
+            
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while(reader.Read())
+            {
+                Console.WriteLine("Blog Id :" + reader["BlogId"]);
+                Console.WriteLine("Blog Aurthor :" + reader["BlogAurthor"]);
+                Console.WriteLine("Blog Title :" + reader["BlogTitle"]);
+                Console.WriteLine("Blog Content :" + reader["BlogContent"]);
+                Console.WriteLine("--------------------------------------------------");
+            }
+
+            connection.Close();
+            Console.WriteLine("Connection was closed!");
+        }
+
+    }
+}
--- KMZWDotNetCore.ConsoleApp/Program.cs
+++ KMZWDotNetCore.ConsoleApp/Program.cs
@@ -1,42 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using KMZWDotNetCore.ConsoleApp;
 using System.Data;
 using System.Data.SqlClient;
 
-SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
-sqlConnectionStringBuilder.DataSource = ".";
-sqlConnectionStringBuilder.InitialCatalog = "DotNetTrainingBatch5";
-sqlConnectionStringBuilder.UserID = "sa";
-sqlConnectionStringBuilder.Password = "sasa@123";
+//SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+//sqlConnectionStringBuilder.DataSource = ".";
+//sqlConnectionStringBuilder.InitialCatalog = "DotNetTrainingBatch5";
+//sqlConnectionStringBuilder.UserID = "sa";
+//sqlConnectionStringBuilder.Password = "sasa@123";
 
-SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+//SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
 
-Console.WriteLine("Connection is opening....");
-connection.Open();
-Console.WriteLine("Connection opened");
+//Console.WriteLine("Connection is opening....");
+//connection.Open();
+//Console.WriteLine("Connection opened");
 
-string queryString = "select * from Tbl_Blog where DeleteFlag = 0 ";
-SqlCommand cmd = new SqlCommand(queryString,connection);
-//SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-//DataTable dt = new DataTable();
-//adapter.Fill(dt);
+//string queryString = "select * from Tbl_Blog where DeleteFlag = 0 ";
+//SqlCommand cmd = new SqlCommand(queryString,connection);
 
-SqlDataReader reader = cmd.ExecuteReader();
-while (reader.Read())
-{
-    Console.WriteLine("BlogId: " + reader["BlogId"]);
-    Console.WriteLine("Blog Aurthor: " + reader["BlogAurthor"]);
-    Console.WriteLine("Blog Title: " + reader["BlogTitle"]);
-    Console.WriteLine("Blog Content: " + reader["BlogContent"]);
-}
 
-Console.WriteLine("Connection is closing....");
-connection.Close();
-Console.WriteLine("Connection was closed!");
-
-//foreach (DataRow dr in dt.Rows)
+//SqlDataReader reader = cmd.ExecuteReader();
+//while (reader.Read())
 //{
-//   Console.WriteLine("Blog Id: " + dr["BlogId"]);
-//   Console.WriteLine("Blog Aruthor: " + dr["BlogAurthor"]);
-//    Console.WriteLine("Blog Title: " + dr["BlogTitle"]);
-//    Console.WriteLine("Blog Content: " + dr["BlogContent"]);
+//    Console.WriteLine("BlogId: " + reader["BlogId"]);
+//    Console.WriteLine("Blog Aurthor: " + reader["BlogAurthor"]);
+//    Console.WriteLine("Blog Title: " + reader["BlogTitle"]);
+//    Console.WriteLine("Blog Content: " + reader["BlogContent"]);
 //}
+
+
+
+
+//Console.WriteLine("Connection is closing....");
+//connection.Close();
+//Console.WriteLine("Connection was closed!");
+
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+adoDotNetExample.Read();
+
