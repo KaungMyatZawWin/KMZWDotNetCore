b46b1aeaba6ebca3c82444fbe33af25555584e97
KaungMyatZawWin <kaungmyatzawwin.dev@gmail.com>
2024-09-29 12:46:28 +0630

    feat : rest api update and delete method

--- KMZWDotNetCore.RestApi/Controllers/BlogsController.cs
+++ KMZWDotNetCore.RestApi/Controllers/BlogsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata;
 
 namespace KMZWDotNetCore.RestApi.Controllers
 {
@@ -41,21 +42,67 @@ namespace KMZWDotNetCore.RestApi.Controllers
             return Ok(blog);
         }
 
-        [HttpPut]
-        public IActionResult UpdateBlogs()
+        [HttpPut("{id}")]
+        public IActionResult UpdateBlogs(int id, TblBlog blog)
         {
-            return Ok();
+            var item = _db.TblBlogs.AsNoTracking().FirstOrDefault(x => x.BlogId == id);
+            if (item is null)
+            {
+                return NotFound();
+            }
+
+            item.BlogTitle = blog.BlogTitle;
+            item.BlogAuthor = blog.BlogAuthor;
+            item.BlogContent = blog.BlogContent;
+
+            _db.Entry(item).State = EntityState.Modified;
+            _db.SaveChanges();
+
+            return Ok(item);
         }
 
-        [HttpPatch]
-        public IActionResult PatchBlogs()
+        [HttpPatch("{id}")]
+        public IActionResult PatchBlogs(int id, TblBlog blog)
         {
-            return Ok();
+            var item = _db.TblBlogs.AsNoTracking().FirstOrDefault(x => x.BlogId == id);
+            if (item is null)
+            {
+                return NotFound();
+            }
+
+            if (!string.IsNullOrEmpty(blog.BlogTitle))
+            {
+                item.BlogTitle = blog.BlogTitle;
+            }
+            if (!string.IsNullOrEmpty(blog.BlogAuthor))
+            {
+                item.BlogAuthor = blog.BlogAuthor;
+            }
+            if (!string.IsNullOrEmpty(blog.BlogContent))
+            {
+                item.BlogContent = blog.BlogContent;
+            }
+
+            _db.Entry(item).State = EntityState.Modified;
+            _db.SaveChanges();
+
+            return Ok(item);
         }
 
-        [HttpDelete]
-        public IActionResult DeleteBlogs()
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBlogs(int id, TblBlog blog)
         {
+            var item = _db.TblBlogs.AsNoTracking().FirstOrDefault(x => x.BlogId == id);
+            if (item is null)
+            {
+                return NotFound();
+            }
+
+            item.DeleteFlag = 1;
+            _db.Entry(item).State = EntityState.Modified;
+
+            _db.SaveChanges();
+
             return Ok();
         }
     }
