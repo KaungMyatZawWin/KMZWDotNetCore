a63f979b325ae676a5ce6ec29a9eb5257e809df1
Kaung Myat Zaw Win <kaungmyatzawwin.dev@gmail.com>
2024-10-12 20:46:44 +0630

    feat: api edit method with dapper

--- KMZWDotNetCore.RestApi/Controllers/BlogsDapperController.cs
+++ KMZWDotNetCore.RestApi/Controllers/BlogsDapperController.cs
@@ -63,5 +63,48 @@ namespace KMZWDotNetCore.RestApi.Controllers
 
         }
         #endregion
+
+        #region EditMethod
+        [HttpPatch("{id}")]
+        public IActionResult EditBlog(int id, BlogsViewModel blog)
+        {
+            using (IDbConnection db = new SqlConnection(_connectionStringBuilder.ConnectionString))
+            {
+                string findquery = "select * from Tbl_Blog where BlogId=@BlogId";
+                var foundBlog = db.QueryFirstOrDefault<BlogsDataModel>(findquery, new { BlogId = id });
+                if (foundBlog is null)
+                {
+                    return BadRequest("Blog Not Found!!");
+                }
+                string condition = "";
+
+
+                if (!string.IsNullOrEmpty(blog.Author))
+                {
+                    condition += "[BlogAuthor] = @BlogAuthor, ";
+                }
+                if (!string.IsNullOrEmpty(blog.Title))
+                {
+                    condition += "[BlogTitle] = @BlogTitle, ";
+                }
+                if (!string.IsNullOrEmpty(blog.Content))
+                {
+                    condition += "[BlogContent] = @BlogContent, ";
+                }
+
+                condition = condition.Substring(0, condition.Length - 2);
+
+                string updateQuery = $@"UPDATE [dbo].[Tbl_Blog]
+                       SET {condition}
+                     WHERE BlogId = @BlogId";
+
+                var model = db.Execute(updateQuery, new { BlogId = id, BlogAuthor = blog.Author, BlogTitle = blog.Title, BlogContent = blog.Content });
+
+                string result = model == 1 ? "Successfully Updated . " : "Failed to update";
+                return Ok(result);
+            };
+
+        }
+        #endregion
     }
 }
