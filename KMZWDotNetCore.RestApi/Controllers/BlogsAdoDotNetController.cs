94eb80d77efb3735105c4cf787da25711b1e7116
Kaung Myat Zaw Win <kaungmyatzawwin.dev@gmail.com>
2024-10-12 20:06:49 +0630

    feat: read method with dapper

--- KMZWDotNetCore.RestApi/Controllers/BlogsAdoDotNetController.cs
+++ KMZWDotNetCore.RestApi/Controllers/BlogsAdoDotNetController.cs
@@ -86,7 +86,6 @@ namespace KMZWDotNetCore.RestApi.Controllers
 
         #endregion
 
-
         #region UpdateMethod
 
         [HttpPut]
---
+++ KMZWDotNetCore.RestApi/Controllers/BlogsDapperController.cs
@@ -0,0 +1,39 @@
+﻿
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace KMZWDotNetCore.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsDapperController : ControllerBase
+    {
+        private readonly SqlConnectionStringBuilder _connectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "DotNetTrainingBatch5",
+            UserID = "sa",
+            Password = "sasa@123",
+            TrustServerCertificate = true,
+        };
+
+        #region ReadMethod
+        [HttpGet]
+        public IActionResult GetBlog()
+        {
+            using (IDbConnection db = new SqlConnection(_connectionStringBuilder.ConnectionString))
+            {
+                string query = "select * from Tbl_Blog where DeleteFlag=0";
+
+                var model = db.Query(query).ToList();
+
+                return Ok(model);
+            }
+
+        }
+        #endregion
+    }
+}
