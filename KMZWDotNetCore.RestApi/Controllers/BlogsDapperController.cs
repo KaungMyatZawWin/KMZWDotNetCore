1ca3789de6a78c6fe43c14a3c9cec541ee1fbce1
Kaung Myat Zaw Win <kaungmyatzawwin.dev@gmail.com>
2024-10-12 20:17:57 +0630

    feat: create api with dapper

--- KMZWDotNetCore.RestApi/Controllers/BlogsDapperController.cs
+++ KMZWDotNetCore.RestApi/Controllers/BlogsDapperController.cs
@@ -1,5 +1,7 @@
 ﻿
 using Dapper;
+using KMZWDotNetCore.RestApi.DataModel;
+using KMZWDotNetCore.RestApi.ViewModel;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
@@ -28,10 +30,36 @@ namespace KMZWDotNetCore.RestApi.Controllers
             {
                 string query = "select * from Tbl_Blog where DeleteFlag=0";
 
-                var model = db.Query(query).ToList();
+                var model = db.Query<BlogsDataModel>(query).ToList();
 
                 return Ok(model);
             }
+
+        }
+        #endregion
+
+        #region CreateMethod
+        [HttpPost]
+        public IActionResult CreatedBlog(BlogsViewModel blog)
+        {
+            using (IDbConnection db = new SqlConnection(_connectionStringBuilder.ConnectionString))
+            {
+                string query = @"INSERT INTO [dbo].[Tbl_Blog]
+                       ([BlogAuthor]
+                       ,[BlogTitle]
+                       ,[BlogContent]
+                       ,[DeleteFlag])
+                 VALUES
+                       (@BlogAuthor
+                       ,@BlogTitle
+                       ,@BlogContent
+                       ,@DeleteFlag)";
+
+                int model = db.Execute(query, new { BlogAuthor = blog.Author, BlogTitle = blog.Title, BlogContent = blog.Content, DeleteFlag = blog.DeleteFlag });
+                var result = model == 1 ? "Successfully Created New Blog. " : "Failed to create!";
+
+                return Ok(result);
+            };
 
         }
         #endregion
