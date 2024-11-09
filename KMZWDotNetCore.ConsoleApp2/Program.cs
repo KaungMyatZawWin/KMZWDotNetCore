019b40d8de9a05e19a7b8e6bc804bf31570c4895
Kaung Myat Zaw Win <kaungmyatzawwin.dev@gmail.com>
2024-11-09 21:38:48 +0630

    feat: bird minimal api

--- KMZWDotNetCore.ConsoleApp2/Program.cs
+++ KMZWDotNetCore.ConsoleApp2/Program.cs
@@ -1,15 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using KMZWDotNetCore.Database.Models;
+using Newtonsoft.Json;
 
 Console.WriteLine("Hello, World!");
 
-AppDbContext db = new AppDbContext();
-var model = db.TblBlogs.Where(blogs => blogs.DeleteFlag == 0).ToList();
-foreach (var item in model)
+//AppDbContext db = new AppDbContext();
+//var model = db.TblBlogs.Where(blogs => blogs.DeleteFlag == 0).ToList();
+//foreach (var item in model)
+//{
+//    Console.WriteLine(item.BlogId);
+//    Console.WriteLine(item.BlogAuthor);
+//    Console.WriteLine(item.BlogTitle);
+//    Console.WriteLine(item.BlogContent);
+//    Console.WriteLine("------------------------");
+//}
+
+var blog = new BlogModal
 {
-    Console.WriteLine(item.BlogId);
-    Console.WriteLine(item.BlogAuthor);
-    Console.WriteLine(item.BlogTitle);
-    Console.WriteLine(item.BlogContent);
-    Console.WriteLine("------------------------");
+    Id = 1,
+    Title = "Testing",
+    Author = "Testing",
+    Description = "Testing"
+};
+
+//string blogJsonStr = JsonConvert.SerializeObject(blog, Formatting.Indented);
+string jsonString = blog.ToJson();
+Console.WriteLine(jsonString);
+Console.ReadLine();
+
+public class BlogModal
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string Author { get; set; }
+
+    public string Description { get; set; }
+
 }
+
+public static class DevCode
+{
+    public static string ToJson(this object obj)
+    {
+        string jsonStr = JsonConvert.SerializeObject(obj, Formatting.Indented);
+        return jsonStr;
+    }
+
+}
+
