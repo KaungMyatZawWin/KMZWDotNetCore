82af6f66f036078a1307bd51f17e14a11bdea25c
Kaung Myat Zaw Win <kaungmyatzawwin.dev@gmail.com>
2025-03-20 07:33:34 +0630

    feat: MVC pratict again

---
+++ KMZWDotNetCore.MVCApp2/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using KMZWDotNetCore.MVCApp2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace KMZWDotNetCore.MVCApp2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Hello from View Bag.";
+            HomeResponseModel model = new HomeResponseModel()
+            {
+                AlertMessage = "Data transfer with custom model"
+            };
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ KMZWDotNetCore.MVCApp2/Models/HomeResponseModel.cs
@@ -0,0 +1,7 @@
+﻿namespace KMZWDotNetCore.MVCApp2.Models
+{
+    public class HomeResponseModel
+    {
+        public string AlertMessage { get; set; }
+    }
+}
